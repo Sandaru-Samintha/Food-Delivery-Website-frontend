be7e8d1db8c446bdeaf7865c16872e81b352e73e
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2025-07-16 22:44:01 +0530

    Create the navigation bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import { assets } from '../../assets/assets'
+import { CiSearch } from "react-icons/ci";
+import { FaShoppingCart } from "react-icons/fa";
+import { assets } from '../../assets/assets';
+import "./Navbar.css";
+import { useState } from "react";
+
 
 function Navbar() {
+
+  const [menu,setMenu]=useState("Home")
   return (
-    <div>
-      <img src={assets.logo} alt="" style={{width:150,height:100}} />
+    <div className='navbar'>
+      <img src={assets.logo} alt=""  className="logo"  />
+      <ul className='navbar-menu'>
+        <li onClick={()=>setMenu("Home")} className={menu==="Home"?"active":""}>Home</li>
+        <li onClick={()=>setMenu("Menu")} className={menu==="Menu"?"active":""}>Menu</li>
+        <li onClick={()=>setMenu("Mobile App")} className={menu==="Mobile App"?"active":""}>Mobile App</li>
+        <li onClick={()=>setMenu("Contact us")} className={menu==="Contact us"?"active":""}>Contact us</li>
+      </ul>
+      
+      <div className='navbar-right'>
+        <CiSearch className='navbar-icon'/>
+        <div className='navbar-search-icon'>
+          <FaShoppingCart  className='navbar-icon'/>
+          <div className='dot'></div>
+        </div>
+        <button>Sign in</button>
+      </div>
+
     </div>
   )
 }
