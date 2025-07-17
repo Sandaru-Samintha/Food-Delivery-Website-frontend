8f3ed2942c06fb63eece902fa2b4a07e2661ebad
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2025-07-18 01:27:38 +0530

    beautify  The mobile app name

--- src/Components/AppDownload/AppDownload.jsx
+++ src/Components/AppDownload/AppDownload.jsx
@@ -1,17 +1,31 @@
-import React from 'react'
-import './AppDownload.css'
-import { assets } from '../../assets/assets'
+import React from "react";
+import "./AppDownload.css";
+import { assets } from "../../assets/assets";
 
 function AppDownload() {
   return (
-    <div className='app-download' id="app-download">
-      <p>For Better Experience Download <br/> Berry Blue App</p>
+    <div className="app-download" id="app-download">
+      <p>
+        For Better Experience Download <br />{" "}
+        <span
+          style={{
+            background: "linear-gradient(45deg, #c5efcf, #3ee4aa, #5e1bef)",
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
+            fontWeight: "bold"
+            
+          }}
+        >
+          Berry Blue
+        </span>{" "}
+        App
+      </p>
       <div className="app-download-platforms">
         <img src={assets.play_store} alt="" />
         <img src={assets.app_store} alt="" />
       </div>
     </div>
-  )
+  );
 }
 
-export default AppDownload
+export default AppDownload;
