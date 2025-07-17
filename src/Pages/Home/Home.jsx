f27e31f0085309f561dd3b124e58886a511f2b59
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2025-07-17 11:15:29 +0530

    Create the menu section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Home.css"
 import Header from '../../Components/Header/Header'
+import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
 
 function Home() {
+  const [category,setCategory]=useState("All")
   return (
     <div>
       <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
     </div>
   )
 }
