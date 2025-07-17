e65c9bea019ba1e43551432d0376d8be8ab5070e
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2025-07-18 00:35:08 +0530

    Create the AppDownload  Component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import "./Home.css"
 import Header from '../../Components/Header/Header'
 import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../Components/FoodDisplay/FoodDisplay'
+import AppDownload from '../../Components/Appdownload/AppDownload'
 
 function Home() {
   const [category,setCategory]=useState("All")
@@ -11,6 +12,7 @@ function Home() {
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory}/>
       <FoodDisplay category={category}/>
+      <AppDownload/>
     </div>
   )
 }
