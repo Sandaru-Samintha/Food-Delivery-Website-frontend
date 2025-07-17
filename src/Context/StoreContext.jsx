d5ccd0be401d68fa376978224f011c09b9e20aaa
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2025-07-17 20:40:44 +0530

    Implement the food display component

--- src/Context/StoreContext.jsx
+++ src/Context/StoreContext.jsx
@@ -1,19 +1,41 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
+export const StoreContext = createContext(null);
 
+function StoreContextProvider(props) {
+  const [cartItems, setCartItems] = useState({});
 
-export const StoreContext =createContext(null)
+  const addToCart = (itemId) => {
+    //if there is no add items in the cart , newly add the product in the cart
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 })); //if there is the items alredy in the the cart value is increasing by when adding
+    }
+  };
 
-const StoreContextProvider =(props)=>{
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 })); // when we remove the items in the crt
+  };
 
-  const contextValue = {food_list}
-  return(
+  useEffect(()=>{
+    console.log(cartItems);
+  },[cartItems])
+
+  //using useContext we can pass the value in any where (any pages)
+  const contextValue = {
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
+  };
+
+  return (
     <StoreContext.Provider value={contextValue}>
       {props.children}
     </StoreContext.Provider>
-  )
-
-
+  );
 }
-export default StoreContextProvider
+export default StoreContextProvider;
