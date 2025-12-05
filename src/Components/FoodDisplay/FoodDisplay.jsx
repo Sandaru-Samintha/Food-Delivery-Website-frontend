4b3fa62ff5d677167e60b56252eb44fdb9c53c43
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2025-12-05 15:17:43 +0530

    create some changes

--- src/Components/FoodDisplay/FoodDisplay.jsx
+++ src/Components/FoodDisplay/FoodDisplay.jsx
@@ -1,23 +1,23 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import "./FoodDisplay.css";
 
 import { StoreContext } from "../../Context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
 
-function FoodDisplay({category}) {
+function FoodDisplay({ category }) {
   const { food_list } = useContext(StoreContext);
   return (
     <div className="food-display" id="food-display">
       <h2>Top Dishes Near You</h2>
       <div className="food-display-list">
-        {food_list.map((item, index) => {
+        {food_list.map((item) => {
           {
             console.log(category, item.category);
           }
           if (category === "All" || category === item.category) {
             return (
               <FoodItem
-                key={index}
+                key={item._id}
                 id={item._id}
                 name={item.name}
                 description={item.description}
